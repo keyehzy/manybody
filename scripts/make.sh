42e92f7e55a6f59ea1ef0c7af02c9f799dc565bd
Matheus Sousa <msousa@sjtu.edu.cn>
2026-01-20 14:55:46 +0800

    Remove --verbose

--- scripts/make.sh
+++ scripts/make.sh
@@ -5,11 +5,11 @@ BUILD_DIR="build"
 OPENBLAS_ROOT="$(brew --prefix openblas)"
 
 cmake -B "$BUILD_DIR" -S . -G Ninja \
-  -DCMAKE_BUILD_TYPE=Debug \
+  -DCMAKE_BUILD_TYPE=Release \
   -DCMAKE_CXX_COMPILER="/opt/homebrew/Cellar/gcc/15.2.0/bin/g++-15" \
   -DCMAKE_CXX_FLAGS="-Wall -Wextra" \
   -DOpenBLAS_ROOT="${OPENBLAS_ROOT}"
 
-cmake --build "$BUILD_DIR" --verbose
+cmake --build "$BUILD_DIR"
 
 "$BUILD_DIR/tests/tests"
