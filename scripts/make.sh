fda41e89d73b36a12e47d08d11b3830522a6f55b
Matheus Sousa <msousa@sjtu.edu.cn>
2026-01-22 13:27:21 +0800

    Add MANYBODY_BUILD_TESTS option to make test builds optional
    Tests and third-party dependencies (RapidCheck) are now only built when
    -DMANYBODY_BUILD_TESTS=ON is passed to CMake. Updated CI workflow and
    local build script to explicitly enable tests.

--- scripts/make.sh
+++ scripts/make.sh
@@ -6,10 +6,12 @@ OPENBLAS_ROOT="$(brew --prefix openblas)"
 
 cmake -B "$BUILD_DIR" -S . -G Ninja \
   -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
   -DCMAKE_CXX_COMPILER="/opt/homebrew/Cellar/gcc/15.2.0/bin/g++-15" \
   -DCMAKE_CXX_FLAGS="-Wall -Wextra" \
-  -DOpenBLAS_ROOT="${OPENBLAS_ROOT}"
+  -DOpenBLAS_ROOT="${OPENBLAS_ROOT}" \
+  -DMANYBODY_BUILD_TESTS=ON
 
-cmake --build "$BUILD_DIR"
+cmake --build "$BUILD_DIR" --verbose
 
 "$BUILD_DIR/tests/tests"
